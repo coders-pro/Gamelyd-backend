09c971c49ccbd8d6651ebb1c4f1d903f65ba415b
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2022-03-01 23:06:18 +0100

    built forgot password endpoint

--- controllers/user.go
+++ controllers/user.go
@@ -382,3 +382,38 @@ func Test() gin.HandlerFunc{
 		c.JSON(http.StatusOK, gin.H{"message": "request processed successfullt",  "hasError": false})
 	}
 }
+
+//   Reset Password
+
+  func ForgotPassword() gin.HandlerFunc {
+	  return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error(), "hasError": true})
+			defer cancel()
+			return
+			
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email":user.Email}).Decode(&foundUser)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{"message":"email is incorrect", "hasError": true})
+			return
+		}
+
+		if err == nil {
+			token, refreshToken, _ := helper.GenerateAllTokens(*foundUser.Email, *foundUser.First_name, *foundUser.Last_name, *foundUser.User_type, *&foundUser.User_id)
+
+			passedtoken := token
+
+			helper.ForgotPasswordMail(*foundUser.Email, passedtoken, *foundUser.User_name)
+
+			c.JSON(http.StatusOK, gin.H{"message": foundUser, "hasError": true,"taken": token,"refreshToken": refreshToken})
+		}
+
+	}
+  }
---
+++ helpers/email.go
@@ -0,0 +1,41 @@
+package helper
+
+import (
+	"net/smtp"
+	"fmt"
+
+	templates "github.com/Gameware/templates"
+)
+
+func ForgotPasswordMail(receiver string, token string, username string) {
+	
+	// Sender data.
+	from := "info@shipping-cargo.com"
+	password := "ZzFV@#UqT7JAr"
+  
+	// Receiver email address.
+	to := []string{
+	  "madustanley1@gmail.com",
+	}
+  
+	// smtp server configuration.
+	smtpHost := "shipping-cargo.com"
+	smtpPort := "26"
+  
+	// Message.
+	subject := "Subject: Reset Password\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	body := templates.ForgotPassword(token, username)
+	msg := []byte(subject + mime + body)
+	// Authentication.
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+	
+	// Sending email.
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, msg)
+	if err != nil {
+	  fmt.Println(err)
+	  return
+	}
+	fmt.Println("Email Sent Successfully!")
+
+  }
