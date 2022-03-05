1c4d141b599b9cc3a4a404744edbcf29348605d3
mcbobby <madumcbobby@yahoo.com>
2022-03-05 13:22:32 +0100

    fix register email

--- helpers/email.go
+++ helpers/email.go
@@ -2,42 +2,69 @@ package helper
 
 import (
 	"fmt"
-	"net/smtp"
 
 	templates "github.com/Gameware/templates"
 	mail "github.com/xhit/go-simple-mail/v2"
 )
 
 func ForgotPasswordMail(receiver string, token string, username string) {
+
+	server := mail.NewSMTPClient()
+	server.Host = "shipping-cargo.com"
+	server.Port = 587
+	server.Username = "info@shipping-cargo.com"
+	server.Password = "ZzFV@#UqT7JAr"
+	server.Encryption = mail.EncryptionTLS
+
+	smtpClient, err := server.Connect()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Create email
+	email := mail.NewMSG()
+	email.SetFrom("Mcbobby from Gamelyd <info@shipping-cargo.com>")
+	email.AddTo(receiver)
+	email.SetSubject("Reset Password")
+
+	email.SetBody(mail.TextHTML, templates.ForgotPassword(token, username))
+	// email.AddAttachment("super_cool_file.png")
+
+	// Send email
+	err = email.Send(smtpClient)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	
 	// Sender data.
-	from := "info@shipping-cargo.com"
-	password := "ZzFV@#UqT7JAr"
+	// from := "info@shipping-cargo.com"
+	// password := "ZzFV@#UqT7JAr"
   
-	// Receiver email address.
-	to := []string{
-	  receiver,
-	}
+	// // Receiver email address.
+	// to := []string{
+	//   receiver,
+	// }
   
 	// smtp server configuration.
-	smtpHost := "shipping-cargo.com"
-	smtpPort := "26"
+	// smtpHost := "shipping-cargo.com"
+	// smtpPort := "26"
   
-	// Message.
-	subject := "Subject: Reset Password\n"
-	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-	body := templates.ForgotPassword(token, username)
-	msg := []byte(subject + mime + body)
-	// Authentication.
-	auth := smtp.PlainAuth("", from, password, smtpHost)
+	// // Message.
+	// subject := "Subject: Reset Password\n"
+	// mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	// body := templates.ForgotPassword(token, username)
+	// msg := []byte(subject + mime + body)
+	// // Authentication.
+	// auth := smtp.PlainAuth("", from, password, smtpHost)
 	
-	// Sending email.
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, msg)
-	if err != nil {
-	  fmt.Println(err)
-	  return
-	}
-	fmt.Println("Email Sent Successfully!")
+	// // Sending email.
+	// err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, msg)
+	// if err != nil {
+	//   fmt.Println(err)
+	//   return
+	// }
+	// fmt.Println("Email Sent Successfully!")
 
   }
 
