b6142d3d8162fd65c95e62658d434351fe8f370c
mcbobby <madumcbobby@yahoo.com>
2022-03-09 15:59:54 +0100

    add logo

--- templates/forgotPassword.go
+++ templates/forgotPassword.go
@@ -30,7 +30,7 @@ func ForgotPassword(token string, name string) string {
                       <tr>
                           <td style="text-align:center;">
                             <a href="https://rakeshmandal.com" title="logo" target="_blank">
-                              <img width="60" src="https://i.ibb.co/hL4XZp2/android-chrome-192x192.png" title="logo" alt="logo">
+                              <img width="60" src="https://gamelyd-madumcbobby.vercel.app/images/logos/logo.png" title="logo" alt="logo">
                             </a>
                           </td>
                       </tr>
