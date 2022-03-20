cc30d6f975f613e85e72a1c748b6720bfd94df86
mcbobby <madumcbobby@yahoo.com>
2022-03-20 21:44:51 +0100

    change emails

--- controllers/contactUs.go
+++ controllers/contactUs.go
@@ -55,7 +55,7 @@ func SaveContactUs() gin.HandlerFunc{
 			return
 		}
 		defer cancel()
-		go helper.SendEmail("madumcbobby@yahoo.com", templates.ReportAbuse(contact.Name, contact.Message), "Abuse Report From Gamelyd")
+		go helper.SendEmail("madumcbobby@yahoo.com", templates.ReportAbuse(contact.Name, contact.Message), "Contact From Gamelyd")
 
 		c.JSON(http.StatusOK, gin.H{"message": "Successfull, we will get back to you soon", "data":contact, "hasError": false, "insertId": resultInsertionNumber})
 	}
